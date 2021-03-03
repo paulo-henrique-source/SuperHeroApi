316ff841047fff8c888d10b5d94e8c3e7dfe2575
paulo-henrique-source <paulohenrique1117@hotmail.com>
2021-03-03 13:43:24 -0300

    test

--- src/API/heroAPI.js
+++ src/API/heroAPI.js
@@ -3,13 +3,21 @@ import { superHeroKey } from '../config/env'
 
 export const findByName = async (name) => {
   const request = {
-    url: `https://superheroapi.com/api/1763087293841652/ProdutosCategoria/${name}`,
+    url: `https://superheroapi.com/api/${superHeroKey}/search/${name}`,
     method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
   }
-  const { status, data } = await Send(request)
-  if (status != 200) {
-    console.error('Server blocked')
-  } else {
-    return data.results
+  try {
+    const { status, data } = await Send(request)
+    if (status != 200) {
+      console.error('Server blocked')
+    } else {
+      return data.results
+    }
+  } catch (err) {
+    console.log(err)
   }
 }
